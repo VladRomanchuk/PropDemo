9c346da3a089f92dc24a1516c48c2bd671d5c677
vladrom <wadoz.1995@gmail.com>
2017-04-05 12:18:47 +0300

    Huffman Tree on kotlin language

--- src/util/HuffmanTree.kt
+++ src/util/HuffmanTree.kt
@@ -2,6 +2,7 @@ package util
 
 import java.io.File
 import java.nio.charset.Charset
+import java.text.Format
 import java.util.*
 
 abstract class HuffmanTree(var freq: Int) : Comparable<HuffmanTree> {
@@ -29,8 +30,10 @@ fun buildTree(charFreqs: IntArray): HuffmanTree {
 }
 
 fun printCode(tree: HuffmanTree, prefix: StringBuffer) {
+    val formatter : Formatter = Formatter()
+    val formatterPrefix : Formatter = Formatter()
     when (tree) {
-        is HuffmanLeaf -> println("${tree.value}\t${tree.freq}\t$prefix")
+        is HuffmanLeaf -> println("${tree.value}\t${formatter.format("%10.5s", tree.freq)}\t${formatterPrefix.format("%12.5s", prefix)}")
         is HuffmanNode -> {
             prefix.append('0')
             printCode(tree.left, prefix)
@@ -44,16 +47,16 @@ fun printCode(tree: HuffmanTree, prefix: StringBuffer) {
 }
 
 fun main(args: Array<String>) {
-//    val test = "Mon Portrait Written by the poet at the age of 15. Vous me demandez mon portrait, Mais peint d'apres nature: Mon cher, il sera bientot fait, Quoique en miniature. Je suis un jeune polisson Encore dans les classes; Point sot, je le dis sans facon,Et sans fades grimaces. Oui! il ne fut babillard Ni docteur de Sorbonne,Plus ennuyeux et plus braillardQue moi-meme en personne.Ma taille, a celle des plus longs,Elle n'est point egalee;J'ai le teint frais, les cheveux blonds,Et la tete bouclee.J'aime et le monde et son fracas,Je hais la solitude;J'abhorre et noises et debats,Et tant soit peu l'etude.Spectacles, bals, me plaisent fort,Et d'apres ma pensee, Je dirais ce que j'aime encore,Si je n'etais au Lycee.Apres cela, mon cher ami, L'on peut me reconnaitre, Oui! tel que le bon Dieu me fit, Je veux toujours paraitre.Vrai demon, par l'espieglerie,Vrai singe par sa mine,Beaucoup et trop d'etourderie,Ma foi! voila Pouchekine."
 
     val test = File("./resources/poems.txt").readText(charset = Charsets.UTF_8)
-    print("asdsa  = $test")
     val maxIndex = test.max()!!.toInt() + 1
-    val freqs = IntArray(maxIndex) //256 enough for latin ASCII table, but dynamic size is more fun
+    val freqs = IntArray(maxIndex)
     test.toLowerCase().forEach { freqs[it.toInt()] += 1 }
 
     val tree = buildTree(freqs)
-    println("SYMBOL\tWEIGHT\tHUFFMAN CODE")
+    val stringFormatter :Formatter = Formatter()
+
+    println("SYMBOL\tWEIGHT\t${stringFormatter.format("%15.12s", "HUFFMAN CODE")}")
     printCode(tree, StringBuffer())
 
     File("./resources/outputBinary.txt").writeText("Kotlin perfect write to file ", Charset.defaultCharset())
